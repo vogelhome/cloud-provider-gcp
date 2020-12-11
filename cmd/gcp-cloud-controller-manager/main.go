daa91983150ba42e9daeb696e469a0b052cffb93
Rafael Franzke <rafael.franzke@sap.com>
2020-12-11 16:51:55 +0100

    Update k8s.io/legacy-cloud-providers to v0.20.0
    ```feature dependency
    `k8s.io/legacy-cloud-providers` is now updated to `v0.20.0`.
    ```

--- cmd/gcp-cloud-controller-manager/main.go
+++ cmd/gcp-cloud-controller-manager/main.go
@@ -14,48 +14,26 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// aws-cloud-controller-manager is responsible for running controller loops
-// that create, delete and monitor cloud resources on AWS. These cloud
-// resources include EC2 instances and autoscaling groups, along with network
-// load balancers (NLB) and application load balancers (ALBs) The cloud
-// resources help provide a place for both control plane components -- e.g. EC2
-// instances might house Kubernetes worker nodes -- as well as data plane
-// components -- e.g. a Kubernetes Ingress object might be mapped to an EC2
-// application load balancer.
-
 package main
 
 import (
 	"fmt"
 	"math/rand"
-	"net"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/spf13/cobra"
-	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/wait"
-	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	cloudprovider "k8s.io/cloud-provider"
-	nodecontroller "k8s.io/cloud-provider/controllers/node"
-	nodelifecyclecontroller "k8s.io/cloud-provider/controllers/nodelifecycle"
-	routecontroller "k8s.io/cloud-provider/controllers/route"
-	servicecontroller "k8s.io/cloud-provider/controllers/service"
+	"k8s.io/cloud-provider/app"
+	"k8s.io/cloud-provider/options" // add the kubernetes feature gates
 	cliflag "k8s.io/component-base/cli/flag"
 	"k8s.io/component-base/cli/globalflag"
-	"k8s.io/component-base/featuregate"
 	"k8s.io/component-base/logs"
 	_ "k8s.io/component-base/metrics/prometheus/clientgo" // for client metric registration
 	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
 	"k8s.io/klog"
-	"k8s.io/kubernetes/cmd/cloud-controller-manager/app"
-	cloudcontrollerconfig "k8s.io/kubernetes/cmd/cloud-controller-manager/app/config"
-	"k8s.io/kubernetes/cmd/cloud-controller-manager/app/options"
-	"k8s.io/kubernetes/pkg/features" // add the kubernetes feature gates
 	"k8s.io/legacy-cloud-providers/gce"
-	netutils "k8s.io/utils/net"
 )
 
 var version string
@@ -66,10 +44,13 @@ func main() {
 	logs.InitLogs()
 	defer logs.FlushLogs()
 
+	controllerList := []string{"cloud-node", "cloud-node-lifecycle", "service", "route"}
+
 	s, err := options.NewCloudControllerManagerOptions()
 	if err != nil {
 		klog.Fatalf("unable to initialize command options: %v", err)
 	}
+	s.KubeCloudShared.CloudProvider.Name = "gce"
 
 	command := &cobra.Command{
 		Use:  "gcp-cloud-controller-manager",
@@ -89,13 +70,24 @@ func main() {
 
 			cliflag.PrintFlags(cmd.Flags())
 
-			c, err := s.Config(KnownControllers(), ControllersDisabledByDefault.List())
+			c, err := s.Config(controllerList, app.ControllersDisabledByDefault.List())
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "%v\n", err)
 				os.Exit(1)
 			}
 
-			if err := app.Run(c.Complete(), wait.NeverStop); err != nil {
+			cloudconfig := c.Complete().ComponentConfig.KubeCloudShared.CloudProvider
+			cloud, err := cloudprovider.InitCloudProvider(cloudconfig.Name, cloudconfig.CloudConfigFile)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+			if cloud == nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+				os.Exit(1)
+			}
+
+			if err := app.Run(c.Complete(), app.DefaultControllerInitializers(c.Complete(), cloud), wait.NeverStop); err != nil {
 				fmt.Fprintf(os.Stderr, "%v\n", err)
 				os.Exit(1)
 			}
@@ -103,7 +95,7 @@ func main() {
 	}
 
 	fs := command.Flags()
-	namedFlagSets := s.Flags(KnownControllers(), ControllersDisabledByDefault.List())
+	namedFlagSets := s.Flags(controllerList, app.ControllersDisabledByDefault.List())
 	globalflag.AddGlobalFlags(namedFlagSets.FlagSet("global"), command.Name())
 
 	for _, f := range namedFlagSets.FlagSets {
@@ -114,153 +106,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-// initFunc is used to launch a particular controller.  It may run additional "should I activate checks".
-// Any error returned will cause the controller process to `Fatal`
-// The bool indicates whether the controller was enabled.
-type initFunc func(ctx *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface, featureGate featuregate.FeatureGate, stop <-chan struct{}) (debuggingHandler http.Handler, enabled bool, err error)
-
-// KnownControllers indicate the default controller we are known.
-func KnownControllers() []string {
-	ret := sets.StringKeySet(newControllerInitializers())
-	return ret.List()
-}
-
-// ControllersDisabledByDefault is the controller disabled default when starting cloud-controller managers.
-var ControllersDisabledByDefault = sets.NewString()
-
-// newControllerInitializers is a private map of named controller groups (you can start more than one in an init func)
-// paired to their initFunc.  This allows for structured downstream composition and subdivision.
-func newControllerInitializers() map[string]initFunc {
-	controllers := map[string]initFunc{}
-	controllers["cloud-node"] = startCloudNodeController
-	controllers["cloud-node-lifecycle"] = startCloudNodeLifecycleController
-	controllers["service"] = startServiceController
-	controllers["route"] = startRouteController
-	return controllers
-}
-
-func startCloudNodeController(ctx *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface, featureGate featuregate.FeatureGate, stopCh <-chan struct{}) (http.Handler, bool, error) {
-	// Start the CloudNodeController
-	nodeController, err := nodecontroller.NewCloudNodeController(
-		ctx.SharedInformers.Core().V1().Nodes(),
-		// cloud node controller uses existing cluster role from node-controller
-		ctx.ClientBuilder.ClientOrDie("node-controller"),
-		cloud,
-		ctx.ComponentConfig.NodeStatusUpdateFrequency.Duration,
-	)
-	if err != nil {
-		klog.Warningf("failed to start cloud node controller: %s", err)
-		return nil, false, nil
-	}
-
-	go nodeController.Run(stopCh)
-
-	return nil, true, nil
-}
-
-func startCloudNodeLifecycleController(ctx *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface, featureGate featuregate.FeatureGate, stopCh <-chan struct{}) (http.Handler, bool, error) {
-	// Start the cloudNodeLifecycleController
-	cloudNodeLifecycleController, err := nodelifecyclecontroller.NewCloudNodeLifecycleController(
-		ctx.SharedInformers.Core().V1().Nodes(),
-		// cloud node lifecycle controller uses existing cluster role from node-controller
-		ctx.ClientBuilder.ClientOrDie("node-controller"),
-		cloud,
-		ctx.ComponentConfig.KubeCloudShared.NodeMonitorPeriod.Duration,
-	)
-	if err != nil {
-		klog.Warningf("failed to start cloud node lifecycle controller: %s", err)
-		return nil, false, nil
-	}
-
-	go cloudNodeLifecycleController.Run(stopCh)
-
-	return nil, true, nil
-}
-
-func startServiceController(ctx *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface, featureGate featuregate.FeatureGate, stopCh <-chan struct{}) (http.Handler, bool, error) {
-	// Start the service controller
-	serviceController, err := servicecontroller.New(
-		cloud,
-		ctx.ClientBuilder.ClientOrDie("service-controller"),
-		ctx.SharedInformers.Core().V1().Services(),
-		ctx.SharedInformers.Core().V1().Nodes(),
-		ctx.ComponentConfig.KubeCloudShared.ClusterName,
-		featureGate,
-	)
-	if err != nil {
-		// This error shouldn't fail. It lives like this as a legacy.
-		klog.Errorf("Failed to start service controller: %v", err)
-		return nil, false, nil
-	}
-
-	go serviceController.Run(stopCh, int(ctx.ComponentConfig.ServiceController.ConcurrentServiceSyncs))
-
-	return nil, true, nil
-}
-
-func startRouteController(ctx *cloudcontrollerconfig.CompletedConfig, cloud cloudprovider.Interface, featureGate featuregate.FeatureGate, stopCh <-chan struct{}) (http.Handler, bool, error) {
-	if !ctx.ComponentConfig.KubeCloudShared.AllocateNodeCIDRs || !ctx.ComponentConfig.KubeCloudShared.ConfigureCloudRoutes {
-		klog.Infof("Will not configure cloud provider routes for allocate-node-cidrs: %v, configure-cloud-routes: %v.", ctx.ComponentConfig.KubeCloudShared.AllocateNodeCIDRs, ctx.ComponentConfig.KubeCloudShared.ConfigureCloudRoutes)
-		return nil, false, nil
-	}
-
-	// If CIDRs should be allocated for pods and set on the CloudProvider, then start the route controller
-	routes, ok := cloud.Routes()
-	if !ok {
-		klog.Warning("configure-cloud-routes is set, but cloud provider does not support routes. Will not configure cloud provider routes.")
-		return nil, false, nil
-	}
-
-	// failure: bad cidrs in config
-	clusterCIDRs, dualStack, err := processCIDRs(ctx.ComponentConfig.KubeCloudShared.ClusterCIDR)
-	if err != nil {
-		return nil, false, err
-	}
-
-	// failure: more than one cidr and dual stack is not enabled
-	if len(clusterCIDRs) > 1 && !utilfeature.DefaultFeatureGate.Enabled(features.IPv6DualStack) {
-		return nil, false, fmt.Errorf("len of ClusterCIDRs==%v and dualstack feature is not enabled", len(clusterCIDRs))
-	}
-
-	// failure: more than one cidr but they are not configured as dual stack
-	if len(clusterCIDRs) > 1 && !dualStack {
-		return nil, false, fmt.Errorf("len of ClusterCIDRs==%v and they are not configured as dual stack (at least one from each IPFamily", len(clusterCIDRs))
-	}
-
-	// failure: more than cidrs is not allowed even with dual stack
-	if len(clusterCIDRs) > 2 {
-		return nil, false, fmt.Errorf("length of clusterCIDRs is:%v more than max allowed of 2", len(clusterCIDRs))
-	}
-
-	routeController := routecontroller.New(
-		routes,
-		ctx.ClientBuilder.ClientOrDie("route-controller"),
-		ctx.SharedInformers.Core().V1().Nodes(),
-		ctx.ComponentConfig.KubeCloudShared.ClusterName,
-		clusterCIDRs,
-	)
-	go routeController.Run(stopCh, ctx.ComponentConfig.KubeCloudShared.RouteReconciliationPeriod.Duration)
-
-	return nil, true, nil
-}
-
-// processCIDRs is a helper function that works on a comma separated cidrs and returns
-// a list of typed cidrs
-// a flag if cidrs represents a dual stack
-// error if failed to parse any of the cidrs
-func processCIDRs(cidrsList string) ([]*net.IPNet, bool, error) {
-	cidrsSplit := strings.Split(strings.TrimSpace(cidrsList), ",")
-
-	cidrs, err := netutils.ParseCIDRs(cidrsSplit)
-	if err != nil {
-		return nil, false, err
-	}
-
-	// if cidrs has an error then the previous call will fail
-	// safe to ignore error checking on next call
-	dualstack, _ := netutils.IsDualStackCIDRs(cidrs)
-
-	return cidrs, dualstack, nil
 }
